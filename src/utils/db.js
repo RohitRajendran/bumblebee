d049466933f2002bbad8ad577235efe05c606761
Rohit Rajendran <rohit.rajendran@outlook.com>
2020-05-10 18:42:02 -0400

    Update env variables

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,10 +2,12 @@ const admin = require("firebase-admin");
 const { pressBuzz } = require("../utils/phone");
 
 admin.initializeApp({
-  credential: admin.credential.cert(
-    JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY)
-  ),
-  databaseURL: process.env.DATABASE_URL,
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  }),
+  databaseURL: process.env.FIREBASE_URL,
 });
 const db = admin.database();
 const ref = db.ref();
